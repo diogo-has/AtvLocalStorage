32f208fee294744d52e329a9dad18f4229ad03d8
diogo-has <dioglois.games@gmail.com>
2024-03-25 09:38:28 -0300

    gerarDoc

--- Carrinho/script.js
+++ Carrinho/script.js
@@ -37,4 +37,41 @@ $(document).ready(function () {
     exibirCarrinho();
 });
 
-const succesClose = () => document.getElementById('pedido').style.display = 'none';
+function gerarDoc() {
+    const listaElement = document.getElementById('lista');
+    const totalElement = document.getElementById('total');
+
+    const listaClone = listaElement.cloneNode(true);
+
+    $(listaClone).find('button').remove();
+
+    const listaHtml = listaClone.innerHTML;
+    const totalHtml = totalElement.innerHTML;
+
+    const conteudoHtml = `
+        <html>
+            <head>
+                <meta charset="UTF-8"/>
+            </head>
+            <body>
+                <h1>Pedido Confirmado</h1>
+                <h3>Agradecemos sua preferência</h3>
+                <br>
+                ${listaHtml}
+                <br>
+                <br>
+                ${totalHtml}
+            </body>
+        </html>
+    `;
+
+    const blob = new Blob([conteudoHtml], {type: "application/msword"});
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "carrinho.doc";
+    link.click();
+    document.getElementById('pedido').style.display = 'block';
+
+}
+
+const successClose = () => document.getElementById('pedido').style.display = 'none';
